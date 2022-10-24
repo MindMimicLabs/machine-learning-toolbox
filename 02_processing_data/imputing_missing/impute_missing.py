17be2728b127a46621e9399108daf8da9df49467
jkorn81 <47157865+jkorn81@users.noreply.github.com>
2022-10-24 15:36:23 -0400

    Add files via upload

--- 02_processing_data/imputing_missing/impute_missing.py
+++ 02_processing_data/imputing_missing/impute_missing.py
@@ -1,24 +1,15 @@
-
-# Replace Outliers with the Median ----
-# Just Change the Function to what you want to replace identified outliers with like mean ----
 from sklearn.datasets import load_iris
 iris = load_iris()
 import pandas as pd
 import numpy as np
 df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                   columns= iris['feature_names'] + ['Species'])
+                  
 names = list(df.columns)
 rang = range(0,len(names))
 for i in rang:
   missing_col = [names[i]]
-  median = float(df[missing_col].median())
-  df[missing_col] = np.where(df[missing_col] > median, median, df[missing_col])
-
-
+  for i in missing_col:
+    df.loc[df.loc[:,i].isnull(),i]=df.loc[:,i].mean()
+    
 df.head()
-
-
-
-
-
-
